2e50536670998e0b978e992c7fdf018ce86e6bbc
phanisravan777 <165199575+phanisravan777@users.noreply.github.com>
2024-04-08 16:24:48 -0400

    Delete test_file.py

--- test_file.py
+++
@@ -1,8 +0,0 @@
-# Basic Test Cases
-
-def test_isPalindrome():
-    assert isPalindrome('madam') == True
-    assert isPalindrome('hello') == False
-    assert isPalindrome('level') == True
-    assert isPalindrome('radar') == True
-    assert isPalindrome('python') == False
