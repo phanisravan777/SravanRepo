20ef8e426f84d7cfefe993c58a5860ee0b3571e4
phanisravan777 <165199575+phanisravan777@users.noreply.github.com>
2024-04-08 12:45:14 -0400

    Delete test_isPalindrome.py

--- test_isPalindrome.py
+++
@@ -1,8 +0,0 @@
-def test_isPalindrome():
-    assert isPalindrome("madam") == True
-    assert isPalindrome("12321") == True
-    assert isPalindrome("hello") == False
-    assert isPalindrome("") == True
-    assert isPalindrome("123456") == False
-
-test_isPalindrome()
