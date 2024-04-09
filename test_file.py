6ef543c6e1814f9b49ba6d44db83be40b1d440b5
phanisravan777 <phanisravan403@gmail.com>
2024-04-09 15:55:05 -0400

    Added test_file.py with isPalindrome test cases

---
+++ test_file.py
@@ -0,0 +1,8 @@
+# Basic Test Cases
+
+def test_is_palindrome():
+    assert isPalindrome('madam') == True
+    assert isPalindrome('hello') == False
+    assert isPalindrome('12321') == True
+    assert isPalindrome('radar') == True
+    assert isPalindrome('python') == False
