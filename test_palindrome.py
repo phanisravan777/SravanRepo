da607c5f3d9ae91a86bd15c340b442ef11807a46
phanisravan777 <phanisravan403@gmail.com>
2024-04-18 16:50:06 -0400

    Add test_palindrome.py file

---
+++ test_palindrome.py
@@ -0,0 +1,16 @@
+# Basic Test Cases
+
+# Test Case 1
+print(isPalindrome('madam')) # Expected Output: True
+
+# Test Case 2
+print(isPalindrome('hello')) # Expected Output: False
+
+# Test Case 3
+print(isPalindrome('level')) # Expected Output: True
+
+# Test Case 4
+print(isPalindrome('radar')) # Expected Output: True
+
+# Test Case 5
+print(isPalindrome('python')) # Expected Output: False
