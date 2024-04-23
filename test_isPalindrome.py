64095681e9c66ec5427d40eb812b309926859c2e
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-23 15:55:48 -0400

    Delete test_isPalindrome.py

--- test_isPalindrome.py
+++
@@ -1,9 +0,0 @@
-# Test Cases for isPalindrome
-
-def test_isPalindrome():
-    assert isPalindrome('madam') == True
-    assert isPalindrome('racecar') == True
-    assert isPalindrome('hello') == False
-    assert isPalindrome('12321') == True
-    assert isPalindrome('radar') == True
-    assert isPalindrome('level') == True
