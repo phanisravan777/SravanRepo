e1c4c30e808904fd73fb62269bbde6232a13cb85
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-23 15:56:57 -0400

    Delete test_cases.py

--- test_cases.py
+++
@@ -1,7 +0,0 @@
-# Test cases for isPalindrome function
-def test_isPalindrome():
-    assert isPalindrome('madam') == True
-    assert isPalindrome('hello') == False
-    assert isPalindrome('level') == True
-    assert isPalindrome('radar') == True
-    assert isPalindrome('python') == False
