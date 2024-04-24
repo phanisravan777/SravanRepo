b49362ec1e12cb21e5ea51c10648307bbc3b2fa2
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-23 20:31:36 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,6 @@
-def isPalindrome(str):
-    for i in range(0, int(len(str)/2)):
-        if str[i] != str[len(str)-i-1]:
-            return False
-    return True
-
-def test_is_palindrome():
-    input_data = [['madam'], ['hello'], ['level'], ['radar'], ['python']]
-    expected_output = [[True, False, True, True, False]]
-    for i in range(len(input_data)):
-        assert isPalindrome(input_data[i][0]) == expected_output[0][i], f'Test case {i+1} failed'
-
-test_is_palindrome()
+#To Check given string is palindrome or not
+def isPalindrome(string): 
+    if (string == string[::-1]) : 
+        return "The string is a palindrome." 
+    else: 
+        return "The string is not a palindrome." 
