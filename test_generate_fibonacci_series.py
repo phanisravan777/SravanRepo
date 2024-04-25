054321cda75f143f0e32737ff97f7be65cea015e
phanisravan777 <phanisravan403@gmail.com>
2024-04-25 17:48:23 -0400

    Added test cases for generate_fibonacci_series

---
+++ test_generate_fibonacci_series.py
@@ -0,0 +1,7 @@
+# Test cases for generate_fibonacci_series
+
+def test_generate_fibonacci_series():
+    assert generate_fibonacci_series(0) == []
+    assert generate_fibonacci_series(1) == [0]
+    assert generate_fibonacci_series(2) == [0, 1]
+    assert generate_fibonacci_series(5) == [0, 1, 1, 2, 3]
