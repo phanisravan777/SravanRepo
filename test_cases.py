4ecffa99bdac048add5424b6e4df03c329ea20c3
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-26 13:49:26 -0400

    Delete test_cases.py

--- test_cases.py
+++
@@ -1,8 +0,0 @@
-# Test cases for isPalindrome method
-
-def test_isPalindrome():
-    assert isPalindrome('madam') == 'The string is a palindrome.'
-    assert isPalindrome('hello') == 'The string is not a palindrome.'
-    assert isPalindrome('12321') == 'The string is a palindrome.'
-    assert isPalindrome('racecar') == 'The string is a palindrome.'
-    assert isPalindrome('python') == 'The string is not a palindrome.'
