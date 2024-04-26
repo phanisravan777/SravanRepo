a8d2c9b7d27027d93ed403c516e9e21bac8b923a
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-26 15:40:57 -0400

    Delete test_cases.py

--- test_cases.py
+++
@@ -1,7 +0,0 @@
-# Regression Test Cases for isPalindrome function
-
-# Test Case 1: Input is a palindrome
-assert isPalindrome('madam') == 'The string is a palindrome.'
-
-# Test Case 2: Input is not a palindrome
-assert isPalindrome('hello') == 'The string is not a palindrome.'
