64549434f4c32d5936a2692da52dd6c9bc2cac19
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-26 14:15:48 -0400

    Delete test_isPalindrome.py

--- test_isPalindrome.py
+++
@@ -1,7 +0,0 @@
-# Regression Test Cases for isPalindrome function
-
-def test_isPalindrome():
-    assert isPalindrome('madam') == 'The string is a palindrome.'
-    assert isPalindrome('hello') == 'The string is not a palindrome.'
-    assert isPalindrome('level') == 'The string is a palindrome.'
-    assert isPalindrome('radar') == 'The string is a palindrome.'
