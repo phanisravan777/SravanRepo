8700cf6fb26fc5a32c43b3a3ee3dc51c234f547c
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-26 15:17:31 -0400

    Delete UnitTests.py

--- UnitTests.py
+++
@@ -1,6 +0,0 @@
-# Basic Test Cases
-def test_generate_fibonacci_series_unitTest():
-    assert generate_fibonacci_series(0) == []
-    assert generate_fibonacci_series(1) == [0]
-    assert generate_fibonacci_series(2) == [0, 1]
-    assert generate_fibonacci_series(5) == [0, 1, 1, 2, 3]
