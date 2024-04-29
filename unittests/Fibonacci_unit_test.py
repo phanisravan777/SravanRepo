990337fe62ecee6e71f9243bc851f240a7ec9c29
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-29 16:57:09 -0400

    Delete unittests directory

--- unittests/Fibonacci_unit_test.py
+++
@@ -1,7 +0,0 @@
-def test_Fibonacci():
-    assert Fibonacci(0) == 0
-    assert Fibonacci(1) == 1
-    assert Fibonacci(2) == 1
-    assert Fibonacci(3) == 2
-    assert Fibonacci(4) == 3
-    assert Fibonacci(5) == 5
