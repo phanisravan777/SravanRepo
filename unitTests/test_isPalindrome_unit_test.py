580fd8c3a34fe7f0fdd3d4d3c106d4cd59d98f21
phanisravan777 <phanisravan403@gmail.com>
2024-04-29 12:45:40 -0400

    commit

---
+++ unitTests/test_isPalindrome_unit_test.py
@@ -0,0 +1,6 @@
+def test_isPalindrome():
+    assert isPalindrome('madam') == 'The string is a palindrome.'
+    assert isPalindrome('hello') == 'The string is not a palindrome.'
+    assert isPalindrome('12321') == 'The string is a palindrome.'
+    assert isPalindrome('racecar') == 'The string is a palindrome.'
+    assert isPalindrome('python') == 'The string is not a palindrome.'
