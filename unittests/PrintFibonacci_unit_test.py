33dcecfb564cc50ee84e2526cf1cb0fb1e2dacb2
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-29 15:34:02 -0400

    Delete unittests directory

--- unittests/PrintFibonacci_unit_test.py
+++
@@ -1,5 +0,0 @@
-def PrintFibonacci(n):
-    fib_sequence = [0, 1]
-    while len(fib_sequence) < n:
-        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-    return fib_sequence
--- unittests/is_palindrome_unit_test.py
+++
@@ -1,3 +0,0 @@
-def is_palindrome(s):
-    s = ''.join(e for e in s if e.isalnum()).lower()
-    return s == s[::-1]
