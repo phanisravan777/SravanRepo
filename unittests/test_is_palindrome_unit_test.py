d293b37e1b3060d1404ea75025b22489f308869d
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-29 17:29:32 -0400

    Delete unittests directory

--- unittests/test_is_palindrome_unit_test.py
+++
@@ -1,4 +0,0 @@
-def test_is_palindrome():
-    assert is_palindrome("A man, a plan, a canal, Panama") == True
-    assert is_palindrome("race a car") == False
-    assert is_palindrome("hello") == False
