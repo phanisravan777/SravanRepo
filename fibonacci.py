77ecb22eb0928de13746600d69561049da94ae85
PhaniSravan <165199575+phanisravan777@users.noreply.github.com>
2024-04-29 15:25:57 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,31 +1,19 @@
-def PrintFibonacci(length):
-    #Initial variable for the base case. 
-    first = 0
-    second = 1
-
-    #Printing the initial Fibonacci number.
-    print(first, second, end=" ")
-
-    #decreasing the length by two because the first 2 Fibonacci numbers 
-    #already printed.
-    length -= 2
-    
-    #Loop until the length becomes 0.
-    while length > 0:
-
-        #Printing the next Fibonacci number.
-        print(first + second, end=" ")
-
-        #Updating the first and second variables for finding the next number. 
-        temp = second
-        second = first + second
-        first = temp
-
-        #Decreasing the length that states the Fibonacci numbers to be 
-        #printed more.
-        length -= 1
-
-if __name__ == "__main__":
-    print("Fibonacci Series - ")
-    PrintFibonacci(7)
-    pass
+def Fibonacci(n):
+ 
+    # Check if input is 0 then it will
+    # print incorrect input
+    if n < 0:
+        print("Incorrect input")
+ 
+    # Check if n is 0
+    # then it will return 0
+    elif n == 0:
+        return 0
+ 
+    # Check if n is 1,2
+    # it will return 1
+    elif n == 1 or n == 2:
+        return 1
+ 
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
